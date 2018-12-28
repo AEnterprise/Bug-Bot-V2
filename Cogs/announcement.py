c4730c17a93ab66b9df70e27dfb6a45e22062ee6
Ghoul <artmazingmao@gmail.com>
2018-12-28 08:15:01 -0500

    Allow editing to announcements!
    Believe that the announcements will allow people to create announcements, as well editing it. Will have one final thing to add (role mentionable and non-mentionable which will be a later project)

--- Cogs/announcement.py
+++ Cogs/announcement.py
@@ -36,5 +36,23 @@ class announcement:
         else: 
             await ctx.send("I am unsure of what you are attempting to do.")
 
+    @Checks.is_employee()
+    @commands.command()
+    async def update(self, ctx: commands.Context, role_name, message_id:int, *, new_message):
+        channel = Configuration.get_channel(ctx, role_name)
+        message = await channel.get_message(message_id)
+        modschannel = Configuration.get_channel(ctx, "modinator")
+        if channel is None:
+            return await ctx.send("Are you sure this is in a correct channel?")
+        if ctx.message.channel != modschannel:
+            return await ctx.send("This feature only works in the modinator channel. Sorry about it!")
+        if message != None:
+            try:
+                await message.edit(content=f"{new_message}")
+            except discord.Forbidden:
+                await ctx.send("it appears that my SEND_MESSAGES perms have been revoked and I cannot edit the message.")
+        else:
+            await ctx.send("I'm not really sure what you are trying to do.")
+
 def setup(bot):
     bot.add_cog(announcement(bot))
