77be8ca72ec5eaf3306374ed4f701dc838a81503
AEnterprise <aenterprise2@gmail.com>
2019-01-01 17:47:17 +0100

    minor cleanup, make helpers not require a ctx

--- Cogs/announcement.py
+++ Cogs/announcement.py
@@ -1,10 +1,7 @@
-import asyncio
-import traceback
 import discord
-from discord import utils
 from discord.ext import commands
 
-from Utils import Pages, Utils, Emoji, Configuration
+from Utils import Configuration
 from Utils import Checks
 
 
@@ -16,9 +13,9 @@ class announcement:
     @commands.bot_has_permissions(manage_roles=True)       
     @commands.command()
     async def announce(self, ctx: commands.Context, role_name, *, message):
-        role = Configuration.get_role(ctx, role_name)
-        channel = Configuration.get_channel(ctx, role_name)
-        modschannel = Configuration.get_channel(ctx, "modinator")
+        role = Configuration.get_role(role_name)
+        channel = Configuration.get_channel(role_name)
+        modschannel = Configuration.get_channel("modinator")
 
         if role is None:
             return await ctx.send("This role may be either deleted or not configured properly.")
@@ -39,14 +36,14 @@ class announcement:
     @Checks.is_employee()
     @commands.command()
     async def update(self, ctx: commands.Context, role_name, message_id:int, *, new_message):
-        channel = Configuration.get_channel(ctx, role_name)
-        modschannel = Configuration.get_channel(ctx, "modinator")
+        channel = Configuration.get_channel(role_name)
+        modschannel = Configuration.get_channel("modinator")
         try:
             message = await channel.get_message(message_id)
-        except (discord.Forbidden) as e:
+        except discord.Forbidden as e:
             await ctx.send("Hmmm.. Seems like I no longer have READ_MESSAGES permission for that channel for some reason.")
             return
-        except (discord.NotFound) as e:
+        except discord.NotFound as e:
             await ctx.send("It is possible that you gave me the wrong ID or I cannot find the message in the channel due to either the message or channel being deleted.")
             return
 
