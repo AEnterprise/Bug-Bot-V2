8ef2b54b2ae7e2ab660f2fde2bfdb3ddf4f0c0b9
Jared Neal <jared@discordapp.com>
2019-01-08 03:56:27 -0800

    This code isn't functional, just uploading to github in case I have a chance to work on it from the office.

--- Cogs/Roles.py
+++ Cogs/Roles.py
@@ -12,15 +12,10 @@ from discord.ext import commands
 
 from Utils import Pages, Utils, Emoji, Configuration
 
-assignable_roles = {
+assignable_roles = [
 
-    "ios": 413478048890093579,
-    "android": 411674120196194304,
-    "linux": 413477593107660800,
-    "not employee": 411674095881814017,
-    "employee": 41167406952887091,
-    "admin": 16261117845700608,
-    }
+    "ios", "android", "linux", "not employee", "admin", "employee"
+    ]
 
 
 class Role_Assigning:
@@ -34,9 +29,11 @@ class Role_Assigning:
         roles_user_has = ctx.author.roles
         user_ID = ctx.author.id
         channel_ID = ctx.channel.id
+        Member_to_dm = discord.Member
 
         embed = await self.roles_embed(user_ID, channel_ID, roles_user_has)
-        message = await ctx.send(embed=embed)
+        dm_channel_id = await discord.User.dm_channel(Member_to_dm)
+        message = await dm_channel_id.send(embed=embed)
         for emoji in Emoji.SELF_ASSIGNABLE_ROLES.values():
             await message.add_reaction(emoji)
 
@@ -50,9 +47,7 @@ class Role_Assigning:
         channel = self.bot.get_channel(channel_ID)
         guild = self.bot.get_guild(payload.guild_id)
         member = guild.get_member(user_ID)
-        Message = payload.message_id
-
-        message = await self.bot.get_message(Message)
+        Message_id = payload.message_id
 
         if str(payload.emoji) in Emoji.SELF_ASSIGNABLE_ROLES.values():
             for key, value in Emoji.SELF_ASSIGNABLE_ROLES.items():
@@ -62,7 +57,8 @@ class Role_Assigning:
                     await asyncio.sleep(1)
                     roles_user_has = member.roles
                     new_embed = await self.roles_embed(user_ID, channel_ID, roles_user_has)
-                    await message.edit(embed=embed)
+                    message_object = await channel.get_message(Message_id)
+                    await message_object.edit(embed=new_embed)
                     break
 
     async def on_raw_reaction_remove(self, payload: discord.RawReactionActionEvent):
@@ -74,6 +70,7 @@ class Role_Assigning:
         channel = self.bot.get_channel(channel_ID)
         guild = self.bot.get_guild(payload.guild_id)
         member = guild.get_member(user_ID)
+        Message_id = payload.message_id
 
         if str(payload.emoji) in Emoji.SELF_ASSIGNABLE_ROLES.values():
             for key, value in Emoji.SELF_ASSIGNABLE_ROLES.items():
@@ -82,9 +79,10 @@ class Role_Assigning:
                     await member.remove_roles(apple)
                     await asyncio.sleep(1)
                     roles_user_has = member.roles
-                    await self.roles_embed(user_ID, channel_ID, roles_user_has)
+                    new_embed = await self.roles_embed(user_ID, channel_ID, roles_user_has)
+                    message_object = await channel.get_message(Message_id)
+                    await message_object.edit(embed=new_embed)
                     break
-        await reaction.message.edit(embed=embed)
 
     async def roles_embed(self, user_ID, channel_ID, roles_user_has):
 
@@ -94,13 +92,13 @@ class Role_Assigning:
         role_list = "Here are the roles you have.Use the reactions below to add the relevant role to yourself on REACTION ADD. To remove the role from yourself, REMOVE the relevant reaction\n"
 
         for has_role in roles_user_has:
-            if has_role.name.lower() in assignable_roles.keys():
+            if has_role.name.lower() in assignable_roles:
                 emoji_to_add = Emoji.SELF_ASSIGNABLE_ROLES[has_role.name.lower()]
                 role_list += f"<:approve:528377465618563102> - {emoji_to_add}" + str(has_role)+ "\n"
                 pass
             else:
                 pass
-        for key in assignable_roles.keys():
+        for key in assignable_roles:
             if key.lower() not in role_list.lower():
                 emoji_to_add = Emoji.SELF_ASSIGNABLE_ROLES[key.lower()]
                 role_list += f"<:deny:528385167493038080>  - {emoji_to_add}" + str(key) + "\n"
