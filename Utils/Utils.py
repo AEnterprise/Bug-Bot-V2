77e31324e4b20d7529e666d93c6a8af11309214b
unknown <tarek-kingman15@hotmail.com>
2019-01-08 07:40:02 +0100

    Cleaning up some of my mess

--- Utils/Utils.py
+++ Utils/Utils.py
@@ -11,18 +11,15 @@ def fetch_from_disk(filename, alternative=None):
             fetch_from_disk(alternative)
         return dict()
 
-
 def save_to_disk(filename, dict):
     with open(f"{filename}.json", "w") as file:
         json.dump(dict, file, indent=4, skipkeys=True, sort_keys=True)
-
 
 def escape_markdown(text):
     text = str(text)
     for c in ("\\", "`", "*", "_", "~", "<"):
         text = text.replace(c, f"\{c}\u200b")
     return text.replace("@", "@\u200b")
-
 
 async def lockdown_shutdown(bot):
     for key in Configuration.get_master_var("BUGCHANNELS").items():
@@ -39,8 +36,6 @@ async def lockdown_shutdown(bot):
             else:
                 overwrites_everyone.send_messages = False
                 await channel.set_permissions(r, overwrite=overwrites_everyone, reason="Bot restart triggered.")
-
-
 
 async def cleanExit(bot, trigger):
     await BugBotLogging.bot_log(f"Shutdown triggered by {trigger}.")
